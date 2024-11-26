f44b98634f09c3815706b76408d155f7caf4e8d4
Ayoub SAHRAOUI <ayoub.sahraoui.pro@gmail.com>
2024-11-26 20:46:45 +0100

    firebase auth

--- lib/screens/login_ecran.dart
+++ lib/screens/login_ecran.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginEcran extends StatelessWidget {
@@ -6,28 +7,30 @@ class LoginEcran extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Text('User Email: ${snapshot.data!.email}'),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                  },
-                  child: const Text('Sign out'),
-                ),
-                Text('User is logged in'),
-              ],
-            );
-          }
-          return const Center(child: Text('User is not logged in'));
-        });
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  Text('User Email: ${snapshot.data!.email}'),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                    },
+                    child: const Text('Sign out'),
+                  ),
+                  Text('User is logged in'),
+                ],
+              );
+            }
+            return SignInScreen();
+          }),
+    );
   }
 }
